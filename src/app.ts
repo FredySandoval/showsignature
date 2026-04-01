7a328c3b55e1241e7c002ca1190b41586d6b40fb
Fredy Sandoval <shockerovip@gmail.com>
2026-03-31 22:01:19 -0600

    chore: app.ts compatibility

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
 import { runCli } from "./index.js";
 
-await runCli();
+async function main() {
+  await runCli();
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
