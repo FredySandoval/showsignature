1895c9c1f30baa278f42ead94f7fec62f28f0c4f
Fredy Sandoval <shockerovip@gmail.com>
2026-04-13 03:52:12 -0600

    add: pi extension

---
+++ agents/pi/showsignature.ts
@@ -0,0 +1,17 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
+import { createBashTool } from "@mariozechner/pi-coding-agent";
+
+const extensionFilePath = fileURLToPath(import.meta.url);
+const extensionFolderPath = path.dirname(extensionFilePath);
+const webfetchHelp = fs.readFileSync(path.join(extensionFolderPath, "SYSTEM.md"), "utf8");
+
+export default function ddgExtension(pi: ExtensionAPI) {
+  pi.on("before_agent_start", async (event) => {
+    return {
+      systemPrompt: `${event.systemPrompt}\n\n${webfetchHelp}`,
+    };
+  });
+}
