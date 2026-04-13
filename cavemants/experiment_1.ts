0cde974904a69fea31f4526fc6090e875a854a53
Fredy Sandoval <shockerovip@gmail.com>
2026-04-12 20:09:43 -0600

    rem: caveman into it's own package

--- cavemants/experiment_1.ts
+++
@@ -1,8 +0,0 @@
-export { toCaveman } from "./caveman.js";
-export {
-  baseRules,
-  protectEntries,
-  ruleTable,
-  ultraRules,
-} from "./language-rules.js";
-export type { Options, ProtectEntry, Rule } from "./caveman.js";
